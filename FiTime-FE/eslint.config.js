8fe6de5cd68f41abaa74deae0341c75947dd5216
jiwoo <daisy0928@naver.com>
2025-11-14 22:27:37 +0900

    chore: ESLint 및 Prettier 재적용

--- FiTime-FE/eslint.config.js
+++ FiTime-FE/eslint.config.js
@@ -1,43 +1,46 @@
-// ...existing code...
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import ts from '@typescript-eslint/eslint-plugin'
-import prettierPlugin from 'eslint-plugin-prettier'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default defineConfig([
-  globalIgnores(['dist', 'node_modules']),
+  globalIgnores(['dist', 'node_modules', 'eslint.config.js']),
 
   js.configs.recommended,
-  ts.configs.recommended,
+  ...tseslint.configs.recommended,
 
   {
     files: ['**/*.{ts,tsx}'],
+
     ...reactHooks.configs['recommended-latest'],
+
     languageOptions: {
-       parser: '@typescript-eslint/parser',
-       ecmaVersion: 2020,
-       sourceType: 'module',
-       globals: globals.browser,
-       parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: globals.browser,
+      parserOptions: {
         ecmaFeatures: { jsx: true },
-       },
+      },
     },
 
     plugins: {
       'react-refresh': reactRefresh,
-      prettier: prettierPlugin,
     },
 
     rules: {
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      'quotes': 'off',
-      'semi': 'off',
-      'prettier/prettier': ['warn', { singleQuote: true, semi: true, endOfLine: 'lf' }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
+      ],
     },
   },
 
-  prettierPlugin.configs?.recommended ?? {},
-])
+  eslintPluginPrettierRecommended,
+]);
