58623dda778a05d09b8e78337334bf3cb1df3a50
jiwoo <daisy0928@naver.com>
2025-11-14 19:17:55 +0900

    chore: ESLint 및 Prettier 적용

--- FiTime-FE/eslint.config.js
+++ FiTime-FE/eslint.config.js
@@ -1,23 +1,43 @@
+// ...existing code...
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import ts from '@typescript-eslint/eslint-plugin'
+import prettierPlugin from 'eslint-plugin-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules']),
+
+  js.configs.recommended,
+  ts.configs.recommended,
+
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    ...reactHooks.configs['recommended-latest'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+       parser: '@typescript-eslint/parser',
+       ecmaVersion: 2020,
+       sourceType: 'module',
+       globals: globals.browser,
+       parserOptions: {
+        ecmaFeatures: { jsx: true },
+       },
+    },
+
+    plugins: {
+      'react-refresh': reactRefresh,
+      prettier: prettierPlugin,
+    },
+
+    rules: {
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'quotes': 'off',
+      'semi': 'off',
+      'prettier/prettier': ['warn', { singleQuote: true, semi: true, endOfLine: 'lf' }],
     },
   },
+
+  prettierPlugin.configs?.recommended ?? {},
 ])
