02344a8b16e9b274c6ce54a6653f0864c365120d
jiwoo <daisy0928@naver.com>
2025-11-15 19:24:44 +0900

    feat: 방 생성 페이지 시작화면 UI 구현 및 /create 라우터 추가

--- FiTime-FE/src/App.tsx
+++ FiTime-FE/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import CreatePage from './pages/CreatePage';
 
 export default function App() {
   return (
     <>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
     </>
   );
 }
