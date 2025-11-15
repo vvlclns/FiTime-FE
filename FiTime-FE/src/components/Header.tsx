dd530deb1b5f31d42a1910a25f4a3dfbba44abea
jiwoo <daisy0928@naver.com>
2025-11-15 19:15:55 +0900

    feat: Header 컴포넌트 구현 및 Button 컴포넌트의 outline variant 스타일 재정의 - Button outline variant에서 border와 text를 primary 컬러로 설정

---
+++ FiTime-FE/src/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <div className="flex items-center w-[375px] h-[60px] px-5 gap-2.5 border-b border-b-gray-200">
+      <div className="text-violet-700 text-xl font-semibold">FiTime</div>
+    </div>
+  );
+};
