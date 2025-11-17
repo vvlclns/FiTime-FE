237bf1b19ecfaa253c150cc6700cf3be82b03c53
jiwoo <daisy0928@naver.com>
2025-11-17 18:09:44 +0900

    feat: 방 참여 페이지 중첩 라우트 추가 - 부모 경로 /room/:room_id에 EntryLayout 배치 - 자식 라우트로 각 방 참여 페이지 추가 - 기본 자식 라우트(index)로 EntryUser 지정 - 중첩 라우트 구조로 단계별 페이지 전환 지원

--- FiTime-FE/src/App.tsx
+++ FiTime-FE/src/App.tsx
@@ -1,10 +1,22 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import { EntryLayout } from './pages/EntryLayout';
+import { EntryUser } from './pages/EntryUser';
+import { EntryTimetable } from './pages/EntryTimetable';
+import { EntryRank } from './pages/EntryRank';
+import { EntryComplete } from './pages/EntryComplete';
 
 export default function App() {
   return (
     <>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/room/:room_id" element={<EntryLayout />}>
+          <Route index element={<EntryUser />} />
+          <Route path="timetable" element={<EntryTimetable />} />
+          <Route path="rank" element={<EntryRank />} />
+          <Route path="complete" element={<EntryComplete />} />
+        </Route>
+      </Routes>
     </>
   );
 }
